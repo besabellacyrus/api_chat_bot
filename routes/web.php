5d699e67f42a9cee0dbbbd56606a3f114b57458e
Cyrus Besabella <besabellacyrus@gmail.com>
2017-08-26 10:31:58 +0800

    added admin auth

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function() {
-    return response()->json(["messages" => 
-        ["text"=>"sample text1","text"=>"test ones"]
-    ]);
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'AdminController@index');
